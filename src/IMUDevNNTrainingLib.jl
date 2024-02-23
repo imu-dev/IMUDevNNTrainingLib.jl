01933b9e6373b71fe789bee728985c8ea96553b1
Marcin Mider <marcin.mider@gmail.com>
2024-02-23 14:46:52 +0100

    plateau detector

--- src/IMUDevNNTrainingLib.jl
+++ src/IMUDevNNTrainingLib.jl
@@ -4,7 +4,10 @@ using Flux
 using JLD2
 
 include("checkpointer.jl")
+include("plateau_detector.jl")
 
 export Checkpointer, checkpoint, index_of_last_checkpoint, path_to_checkpoint
+
+export PlateauDetector
 
 end
---
+++ src/plateau_detector.jl
@@ -0,0 +1,66 @@
+"""
+A plateau detector for learning rate schedulers.
+It will adjust the learning rate when the loss has stopped improving for a
+number of ticks.
+
+!!! tip
+    Most commonly one tick is equal either to one epoch or one batch, but
+    the user is free to define it as they see fit.
+"""
+@kwdef mutable struct PlateauDetector
+    """
+    The number of consecutive ticks with no improvement before reducing the
+    learning rate.
+    """
+    patience::Int = 10
+    """The factor by which the learning rate will be reduced."""
+    factor::Float64 = 0.1
+    """
+    Internal only. The last tick number that the learning rate was adjusted.
+    """
+    last_tick::Int = 0
+    """
+    Internal only. The best loss found so far.
+    """
+    best_loss::Float32 = Inf32
+    """
+    Internal only. The tick number of the best loss found so far.
+    """
+    best_tick::Int = 0
+    """
+    The minimum learning rate. If the learning rate is reduced to a value
+    smaller than this, it will not be reduced further.
+    """
+    ϵ::Float64 = 1e-8
+    """
+    Internal only. The current learning rate.
+    """
+    η::Float64
+end
+
+"""
+Advance the plateau detector by one tick.
+"""
+function step!(pd::PlateauDetector, loss::Float32)
+    current_tick = pd.last_tick + 1
+    update_learning_rate = if loss < pd.best_loss
+        pd.best_loss = loss
+        pd.best_tick = current_tick
+        false
+    elseif current_tick - pd.best_tick >= pd.patience
+        pd.best_tick = current_tick
+        true
+    else
+        false
+    end
+    pd.last_tick = current_tick
+    return update_learning_rate
+end
+
+function update!(o, pd::PlateauDetector)
+    if pd.η <= pd.ϵ
+        return nothing
+    end
+    pd.η = max(pd.η * pd.factor, pd.ϵ)
+    return Flux.Optimisers.adjust!(o, pd.η)
+end
